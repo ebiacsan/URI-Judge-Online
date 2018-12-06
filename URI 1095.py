2f5cbbddb604af6deca404c8e8042dfda82f6a7c
ebiacsan <32331590+ebiacsan@users.noreply.github.com>
2018-12-06 02:46:45 -0200

    Update URI 1095.py

--- URI 1095.py
+++ URI 1095.py
@@ -1,15 +1,16 @@
-''' *
-    *Fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+''' /*
+     * Fazer um programa que apresente a sequencia conforme o exemplo abaixo.
 
-    *Não tem entrada, mas a saida deve ser:
+     * Não tem entrada, mas a saida deve ser:
 
-    *I=1 J=60
-    *I=4 J=55
-    *I=7 J50
-    *...
-    *I=? J=0
+     * I=1 J=60
+     * I=4 J=55
+     * I=7 J50
+     * ...
+     * I=? J=0
 
-    * '''
+     */ 
+'''
 
 j = 65
 i = -2
