6555885d4d0cfd726c09698afc827bb71074b827
ebiacsan <32331590+ebiacsan@users.noreply.github.com>
2018-12-06 01:35:13 -0200

    Add files via upload

---
+++ 1097.py
@@ -0,0 +1,29 @@
+#Fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+#Não tem entrada, mas a saida deve ser:
+
+#I=1 J=7
+#I=1 J=6
+#I=1 J=5
+#I=3 J=9
+#I=3 J=8
+#I=3 J=7
+#...
+#I=9 J=15
+#I=9 J=14
+#I=9 J=13
+
+
+I, J,F,G = -1,5,4,3
+for i in range (1,6):
+    i=I+2
+    j=J+2
+    f=F+2
+    g=G+2
+    print('I=%d J=%d' %(i,j))
+    print('I=%d J=%d' %(i,f))
+    print('I=%d J=%d' %(i,g))
+    I=i
+    J=j
+    F=f
+    G=g
