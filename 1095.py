26d335c328694818172be37ddd9773b429d59ef9
ebiacsan <32331590+ebiacsan@users.noreply.github.com>
2018-12-06 01:31:23 -0200

    Add files via upload

---
+++ 1095.py
@@ -0,0 +1,24 @@
+#Fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+#Não tem entrada, mas a saida deve ser:
+
+#I=1 J=60
+#I=4 J=55
+#I=7 J50
+#...
+#I=? J=0
+
+
+
+j = 65
+i = -2
+for I in range (1,14):
+    J= j-5
+    I=i+3
+    print('I=%d J=%d' %(I,J))
+    j=J
+    i=I
+
+
+
+
