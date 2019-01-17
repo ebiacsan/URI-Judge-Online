55efaf5d138546a269bdde153916f65cb48422c9
Bianca Csan <32331590+ebiacsan@users.noreply.github.com>
2019-01-17 13:04:47 -0200

    Add files via upload

---
+++ C/URI 1004.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int A, B, prod;
+    
+    scanf("%ld", &A);
+    scanf("%ld", &B);
+    
+    prod = (A*B);
+    
+    printf("PROD = %ld\n", prod);
+
+    return 0;
+}
