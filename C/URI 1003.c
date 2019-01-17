bf93b316f4a8ae13f3b62631cdf9d74eeac21a79
Bianca Csan <32331590+ebiacsan@users.noreply.github.com>
2019-01-17 13:03:02 -0200

    Update URI 1003.c

--- C/URI 1003.c
+++ C/URI 1003.c
@@ -1,29 +1,15 @@
 #include <stdlib.h>
-
 #include <stdio.h>
 
-
-
 int main()
-
 {
-	
 	int A, B;
-	
 	int X;
 	
-	
 	scanf("%d %d", &A, &B);
-	
-	
-	
 	X = A+B;
-	
 	
 	printf("X = %d\n", X);
 	
-	
-	
 	return 0;
-
 }
