7b6e1cbcad55cdd36c8507bacc86b95aefe5c8cc
Bianca Csan <32331590+ebiacsan@users.noreply.github.com>
2019-01-17 13:27:09 -0200

    Update URI 1097.py

--- Python/URI 1097.py
+++ Python/URI 1097.py
@@ -1,3 +1,20 @@
+''' /* 
+     *Fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+     * Não tem entrada, mas a saida deve ser:
+     
+     * I=1 J=7
+     * I=1 J=6
+     * I=1 J=5
+     * I=3 J=9
+     * I=3 J=8
+     * I=3 J=7
+     * ...
+     * I=9 J=15
+     * I=9 J=14
+     * I=9 J=13
+     */
+'''
+
 I, J,F,G = -1,5,4,3
 for i in range (1,6):
     i=I+2
@@ -10,4 +27,4 @@ for i in range (1,6):
     I=i
     J=j
     F=f
-    G=g
+G=g
